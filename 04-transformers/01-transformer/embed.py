816451516ac3b875188cb5516ddcc1b44be8a546
ishaan-ansari <ishaanansari044@gmail.com>
2025-08-14 00:32:54 +0530

    implementing positional encoding

--- 04-transformers/01-transformer/embed.py
+++ 04-transformers/01-transformer/embed.py
@@ -37,6 +37,32 @@ def tokenize_and_embed(word: str, embedding_model) -> list:
     embeddings = numpy.array([get_embedding(word, embedding_model) for word in tokens])
     return embeddings
 
+def add_positional_encoding(embeddings: numpy.ndarray) -> numpy.ndarray:
+    """
+    Add positional encoding to the input embedding.
 
+    Args:
+        embeddings (numpy.ndarray): Input embedding.
 
+    Returns:
+        numpy.ndarray: Embeddings with added positional encoding.
+    """
 
+    sequence_len = embeddings.shape[0]
+    embedding_dim = embeddings.shape[1]
+
+    # initialize positional encoding matrix
+    pos_enc_matrix = np.zeros((sequence_len, embedding_dim))
+
+    # Calculate the positional encodings
+    for pos in range(sequence_len):
+        for i in range(embedding_dim):
+            if i % 2 == 0:
+                pos_enc_matrix[pos, i] = numpy.sin(pos / (10000 ** (2 * i / embedding_dim)))
+            else:
+                pos_enc_matrix[pos, i] = numpy.cos(pos / (10000 ** (2 * i / embedding_dim)))
+
+    # Add positional encodings
+    embedding_matrix = embeddings + pos_enc_matrix
+
+    return embedding_matrix
