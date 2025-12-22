696aad9ed4b10d75166dfd74ace0147c63a74a67
ishaan-ansari <ishaanansari044@gmail.com>
2025-12-23 00:38:05 +0530

    Add basic tensor operations and neural network implementation in PyTorch

---
+++ PyTorch_learning/2_autograd.py
@@ -0,0 +1,17 @@
+"""
+PyTorch automatic differentiation engine (autograd)
+"""
+
+import torch
+import torch.nn.functional as F
+
+y = torch.tensor([1.0])
+x1 = torch.tensor([1.1])
+w1 = torch.tensor([2.2])
+b = torch.tensor([0.5])
+
+z = x1 * w1 + b
+a = torch.sigmoid(z)
+
+loss = F.binary_cross_entropy(a, y)
+print(loss)
---
+++ PyTorch_learning/3_nnet.py
@@ -0,0 +1,30 @@
+"""
+Neural networks in PyTorch
+"""
+import torch
+import torch.nn.functional as F
+
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super().__init__()
+        self.layers = torch.nn.Sequential(
+            # 1st hidden layer
+            torch.nn.Linear(num_inputs, 30),
+            torch.nn.ReLU(),
+
+            # 2nd hidden layer
+            torch.nn.Linear(30, 20),
+            torch.nn.ReLU(),
+
+            # output layer
+            torch.nn.Linear(20, num_outputs),
+        )
+        
+    def forward(self, x):
+        logits = self.layers(x)
+        return logits
+    
+# create a neural network instance
+model = NeuralNetwork(num_inputs=10, num_outputs=1)
+# print the model architecture
+print(model)
