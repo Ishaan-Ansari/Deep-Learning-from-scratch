943955ea9a6b48380eafdb5239da0879f5614e5a
ishaan-ansari <ishaanansari044@gmail.com>
2025-12-23 20:08:57 +0530

    learned about gradient tracking

--- PyTorch_learning/2_autograd.py
+++ PyTorch_learning/2_autograd.py
@@ -7,8 +7,8 @@ import torch.nn.functional as F
 
 y = torch.tensor([1.0])
 x1 = torch.tensor([1.1])
-w1 = torch.tensor([2.2])
-b = torch.tensor([0.5])
+w1 = torch.tensor([2.2], requires_grad=True)    # enable gradient tracking
+b = torch.tensor([0.5], requires_grad=True) 
 
 z = x1 * w1 + b
 a = torch.sigmoid(z)
